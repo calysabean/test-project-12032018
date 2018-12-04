94db4ab3a5a5f05dc931384893b745db23639d8a
calysabean <42517401+calysabean@users.noreply.github.com>
2018-12-04 00:22:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ function getDataFromApi(artist, title, callback) {
     function displaySearchData(data) {
     $('.js-search-results').html(`${data.lyrics}`);
     }
+
+function watchSubmit() {
+$('.js-search-form').submit(function(event){
+  event.preventDefault();
+  let query1 = $(event.currentTarget).find('.js-query-artist');
+  let query2 = $(event.currentTarget).find('.js-query-title');
+  let qVal1 = query1.val();
+  let qVal2 = query2.val();
+  query1.val("");
+  query2.val("");
+  getDataFromApi(qVal1, qVal2, displaySearchData)
+})
+}
+
+$(watchSubmit);
